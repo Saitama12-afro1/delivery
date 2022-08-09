73f79390878e9d1248a0e54a11ee50e2c20a2143
danila05122014 <danila.grom.01@mail.ru>
2022-08-09 18:17:58 +0300

    modified:   API_Delivery/main.py

--- API_Delivery/main.py
+++ API_Delivery/main.py
@@ -1,14 +1,26 @@
 import json
 import requests
+import pprint
+
 API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTEyNDQ2NzMsImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6IlNoYWhtYXRpc3RQaXRvbmlzdCJ9.DVMdTenDBFY0xfUDaRez-V_rEJTD9N5dKZdZSzl64W4"
-url = "https://probe.fbrq.cloud/v1/send/12"
+url = "https://probe.fbrq.cloud/v1/send/1"
 
 json_body = json.dumps({
   "id": 1,
-  "phone" : 124165,
+  "phone" : 0,
   "text": "113143"
 })
+headers = {}
+headers["accept"] = "application/json"
+headers["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTEyNDQ2NzMsImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6IlNoYWhtYXRpc3RQaXRvbmlzdCJ9.DVMdTenDBFY0xfUDaRez-V_rEJTD9N5dKZdZSzl64W4"
+headers["Content-Type"] = "application/json"
 
-responce = requests.post(url=url,json=json_body)
+data = """
+{
+  "id": 2,
+  "phone": 32,
+  "text": "string"
+}"""
+responce = requests.post(url=url,data=json_body,headers=headers)
 
-print(responce.text)
+pprint.pprint(responce.text)
