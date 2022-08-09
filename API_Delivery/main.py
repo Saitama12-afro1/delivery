1b889f58d4e27b7d4109216e87930c65a42d9467
danila05122014 <danila.grom.01@mail.ru>
2022-08-09 16:20:21 +0300

    modified:   API_Delivery/main.py

--- API_Delivery/main.py
+++ API_Delivery/main.py
@@ -1,4 +1,14 @@
+import json
 import requests
-responce = requests.post("https://probe.fbrq.cloud/docs#/send/sendMsg")
+API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTEyNDQ2NzMsImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6IlNoYWhtYXRpc3RQaXRvbmlzdCJ9.DVMdTenDBFY0xfUDaRez-V_rEJTD9N5dKZdZSzl64W4"
+url = "https://probe.fbrq.cloud/v1/send/12"
 
-print(responce.headers)
+json_body = json.dumps({
+  "id": 1,
+  "phone" : 124165,
+  "text": "113143"
+})
+
+responce = requests.post(url=url,json=json_body)
+
+print(responce.text)
